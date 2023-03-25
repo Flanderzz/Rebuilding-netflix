6391b7359660a2e9149a20a504da00a84e9d708f
Ned Flanderzz <devinechinmere@gmail.com>
2023-03-24 23:54:00 -0400

    made file name more clear

--- netflix_clone/pages/auth.tsx
+++ netflix_clone/pages/auth.tsx
@@ -14,7 +14,7 @@ const Auth = () => {
 
 
     return(
-        <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="relative h-full w-full bg-[url('/images/auth_background_img.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-12 py-5">
                     <img src="/images/logo.png" alt="Logo" className="h-12"/>
